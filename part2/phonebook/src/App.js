55f005f0fe6e43295b86c1ce95d17fb97e39be43
Ira Varshavets <varshavets@gmail.com>
2022-07-08 18:03:40 +0200

    Removed test data from phonebook/App.js

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,13 +6,6 @@ import AddForm from './components/AddForm'
 
 const PERSONS_URL = 'http://localhost:3001/persons'
 
-const data =  [
-  { name: 'Arto Hellas', number: '040-123456', id: 1 },
-  { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-  { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-  { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-]
-
 const App = () => {
   const [persons, setPersons] = useState([])
   const [query, setQuery] = useState('')
