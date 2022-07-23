38d77318e3e231e8de26e2b400bbbfe97d90437d
Ira Varshavets <varshavets@gmail.com>
2022-07-23 18:03:07 +0200

    Task 6.1-6.2: Introducing state managements with reducers in the unicafe app

---
+++ part6/unicafe-redux/src/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createStore } from 'redux'
+import reducer from './reducer'
+
+const store = createStore(reducer)
+
+const App = () => {
+  const handleVoteClick = (type) => {
+    store.dispatch({type})
+  }
+
+  return (
+    <div>
+      <button onClick={() => handleVoteClick('GOOD')}>good</button>
+      <button onClick={() => handleVoteClick('OK')}>ok</button>
+      <button onClick={() => handleVoteClick('BAD')}>bad</button>
+      <button onClick={() => handleVoteClick('ZERO')}>reset stats</button>
+      <div>good {store.getState().good}</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+const renderApp = () => {
+  root.render(<App />)
+}
+
+renderApp()
+store.subscribe(renderApp)
