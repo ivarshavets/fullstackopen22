ca555d35f635d2103f376b9a4df347aef610a2b7
Ira Varshavets <varshavets@gmail.com>
2022-09-05 13:21:41 +0200

    Tasks 9.1.-9.3: Add BMI calculator and Excercise calculator wuth TS

---
+++ part9/ts_101/bmiCalculator.ts
@@ -0,0 +1,56 @@
+export const calculateBmi = (height: number, weight: number): string => {
+  const heighIntMeters = height/100;
+  const bmi = weight/ Math.pow(heighIntMeters, 2)
+  let result = ''
+
+  if ( bmi < 16 ) {
+    result = "Underweight (Severe thinness)";
+  } else if ( bmi < 17 ) {
+    result = "Underweight (Moderate thinness)";
+  } else if ( bmi < 18.5) {
+    result = "Underweight (Mild thinness)";
+  } else if ( bmi < 25) {
+    result = "Normal range";
+  } else if ( bmi < 30) {
+    result = "Overweight (Pre-obese)";
+  } else if ( bmi < 35) {
+    result = "Obese (Class I)";
+  } else if ( bmi < 40) {
+    result = "Obese (Class II)";
+  } else {
+    result = "Obese (Class III)";
+  }
+
+  console.log(result);
+  return result;
+};
+
+interface BmiParams {
+  height: number;
+  weight: number;
+}
+
+const parseArguments = (args: Array<string>): BmiParams => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+  if (isNaN(Number(args[2])) || isNaN(Number(args[3]))) {
+    throw new Error('Provided arguments are not numbers')
+  }
+
+  return {
+    height: Number(args[2]),
+    weight: Number(args[3])
+  };
+}
+
+try {
+  // The process.argv() method is used for returning all the command-line arguments that were passed when the Node. js process was being launched.
+  const {height, weight} = parseArguments(process.argv)
+  calculateBmi(height, weight)
+} catch (error: unknown) {
+  let errorMessage = 'Something happend'
+  if (error instanceof Error) {
+    errorMessage += `Error ${error.message}`
+  }
+  console.log(errorMessage)
+}
---
+++ part9/ts_101/exerciseCalculator.ts
@@ -0,0 +1,90 @@
+interface Results {
+  periodLengthInDays: number;
+  trainingDays: number;
+  target: number;
+  average: number;
+  isTargetReached: boolean;
+  rating: number;
+  ratingDescription: string;
+}
+
+export const calculateExercises = (days: Array<number>, target: number): Results => {
+  const getRating = () => {
+    if (average < 1) {
+        return 1
+    }
+    if (average < 2) {
+        return 2
+    }
+    return 3
+  }
+
+  const ratingDescription = {
+    1: 'Not enough exercises',
+    2: 'Not too bad but could be better',
+    3: 'Good job!'
+  };
+
+  const periodLengthInDays = days.length
+
+  const trainingDays = days.reduce((days, dayHrs) => {
+    if (dayHrs !== 0) {
+      days+=1
+    }
+    return days
+  }, 0)
+
+  // const trainingDays = days.filter(d => d>0).length
+
+  const trainingHours = days.reduce((total, hr) => total + hr, 0)
+
+  const average = trainingHours / periodLengthInDays
+
+  const isTargetReached = average >= target
+
+  const rating = getRating()
+
+  return {
+    periodLengthInDays,
+    trainingDays,
+    target,
+    average,
+    isTargetReached,
+    rating,
+    ratingDescription: ratingDescription[rating]
+  }
+}
+
+interface Params {
+  days: Array<number>;
+  target: number;
+}
+
+const parseArguments = (args: Array<string>): Params => {
+  // ts-node exerciseCalculator.ts 2 3 0 2 4.5 0 3 1
+  // ts-node + file name + target + 7 daily exercise hours --> 10 arguments
+  if (args.length < 10) throw new Error('Not enough arguments');
+
+  const funcArgs = args.slice(2)
+  if (funcArgs.find(v => isNaN(Number(v)))) throw new Error('Values should be numbers')
+
+  const funcArgsNumbers = funcArgs.map(v => Number(v))
+
+  return {
+    days: funcArgsNumbers.slice(1),
+    target: funcArgsNumbers[0],
+  }
+}
+
+try {
+  const {days, target} = parseArguments(process.argv)
+  console.log('days', days, 'target', target)
+  const result = calculateExercises(days, target)
+  console.log(result)
+} catch (e: unknown) {
+  let errorMessage = 'Somthing went wrong.'
+  if (e instanceof Error) {
+    errorMessage += `Error: ${e.message}`
+  }
+  console.log(errorMessage)
+}
