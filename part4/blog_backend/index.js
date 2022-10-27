a019627426b7aaa7d379362a23380af346e8b838
Ira Varshavets <varshavets@gmail.com>
2022-10-27 16:34:47 +0200

    Changed node server creation from using express to http

--- part4/blog_backend/index.js
+++ part4/blog_backend/index.js
@@ -1,16 +1,11 @@
 
 const app = require('./app')
+const http = require('http')
 const { PORT } = require('./utils/config')
 const logger = require('./utils/logger')
 
-app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`)
+const server = http.createServer(app)
+
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
-
-
-// const http = require('http')
-// const server = http.createServer(app)
-
-// server.listen(config.PORT, () => {
-//   logger.info(`Server running on port ${config.PORT}`)
-// })
