e5061499949a15ac5333a343e10554c86b7a6360
Ira Varshavets <varshavets@gmail.com>
2023-04-19 14:36:38 +0200

    Task 8.13-8.16 Added models

---
+++ part8/library-backend/models/author.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const uniqueValidator = require('mongoose-unique-validator')
+
+const schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true, // keep validation in the database, but already ensured that the fields exist with GraphQL
+    unique: true,
+    minlength: 4
+  },
+  born: {
+    type: Number,
+  },
+})
+
+schema.plugin(uniqueValidator)
+
+// // In Mongo the identifying field of an object is called _id but there is no need to parse the name of the field to id since GraphQL can do this automatically.
+// schema.set('toJSON', {
+//   transform: (_document, returnedObject) => {
+//     returnedObject.id = returnedObject._id.toString()
+//     delete returnedObject._id
+//     delete returnedObject.__v
+//   }
+// })
+
+module.exports = mongoose.model('Author', schema)
