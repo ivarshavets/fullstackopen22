f00cf9919ca4f03a427f8aabd4ff3ab04f311a7c
Ira Varshavets <varshavets@gmail.com>
2024-04-03 16:58:20 +0200

    Implemented login functionality with LoginForm component

--- part8/library-frontend/src/App.js
+++ part8/library-frontend/src/App.js
@@ -1,12 +1,17 @@
-import { useState, useCallback } from 'react'
+import { useState, useCallback, useEffect } from 'react'
+import { useApolloClient } from '@apollo/client'
 import Notification from './components/Notification'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
+import LoginForm from './components/LoginForm'
+import { STORAGE_KEY } from './utils'
 
 const App = () => {
+  const [token, setToken] = useState(null)
   const [page, setPage] = useState('authors')
   const [flashMessage, setFlashMessage] = useState(null)
+  const client = useApolloClient()
 
   const notify = useCallback((text, type = 'success') => {
     setFlashMessage({text, type})
@@ -15,14 +20,46 @@ const App = () => {
     }, 10000)
   }, [])
 
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem(STORAGE_KEY)
+    if (token) {
+      setToken(token)
+    }
+  }, [])
+
+  if (!token) {
+    return (
+      <div>
+        <Notification message={flashMessage} />
+        <h2>Login</h2>
+        <LoginForm
+          setToken={setToken}
+          setError={notify}
+          setPage={setPage}
+        />
+      </div>
+    )
+  }
+
   return (
     <div>
       <Notification message={flashMessage} />
-      <div>
-        <button onClick={() => setPage('authors')}>authors</button>
-        <button onClick={() => setPage('books')}>books</button>
-        <button onClick={() => setPage('add')}>add book</button>
-      </div>
+      {token &&(
+        <div>
+          <button onClick={() => setPage('authors')}>authors</button>
+          <button onClick={() => setPage('books')}>books</button>
+          <button onClick={() => setPage('add')}>add book</button>
+          <button onClick={logout}>logout</button>
+        </div>
+      )}
+        {/* <button onClick={() => setPage('login')}>login</button> */}
+
 
       <Authors show={page === 'authors'} setError={notify} />
 
---
+++ part8/library-frontend/src/components/LoginForm.js
@@ -0,0 +1,55 @@
+import {useState, useEffect} from 'react'
+import { useMutation } from '@apollo/client'
+import { LOGIN } from '../queries'
+import { STORAGE_KEY } from '../utils'
+
+const LoginForm = ({setToken, setError, setPage}) => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const [ login, result ] = useMutation(LOGIN, {
+    onError: (error) => {
+      setError(error.graphQLErrors[0].message)
+    }
+  })
+
+  useEffect(() => {
+    if ( result.data ) {
+      const token = result.data.login.value
+      setToken(token)
+      setPage('books')
+      localStorage.setItem(STORAGE_KEY, token)
+    }
+  }, [result.data]) // eslint-disable-line
+
+  const submit = async (event) => {
+    event.preventDefault()
+    login({ variables: { username, password } })
+  }
+
+  return (
+    <form onSubmit={submit}>
+      <div>
+        <label>Username</label>
+        <input
+          value={username}
+          onChange={({target: {value}}) => {setUsername(value)}}
+          name="username"
+          type="text"
+        />
+      </div>
+      <div>
+        <label>Password</label>
+        <input
+          value={password}
+          onChange={({target: {value}}) => {setPassword(value)}}
+          name="password"
+          type="password"
+        />
+      </div>
+      <button type='submit'>Login</button>
+    </form>
+  )
+}
+
+export default LoginForm
