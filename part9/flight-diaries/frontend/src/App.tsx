0a32d306008d96c67e9d47f03b86be84f7e49811
Ira Varshavets <varshavets@gmail.com>
2024-07-18 19:42:59 +0200

    Added axios. Added functionality for fetching flight diary entries.

--- part9/flight-diaries/frontend/src/App.tsx
+++ part9/flight-diaries/frontend/src/App.tsx
@@ -1,34 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const URL = 'http://localhost:3000/api'
+
+// types
+interface Flight {
+  id: string,
+  date: string,
+  weather: string,
+  visibility: string
+}
+
+//api
+const getFlights = async () => await axios.get(`${URL}/diaries`)
+// const getFlights = () => axios.get(`${URL}/diaries`)
+
+const fetchFlights = async () => {
+  try {
+    return getFlights().then((result) => {
+      return result.data
+    })
+  } catch(error:unknown) {
+    if(error instanceof Error) {
+      console.log(error)
+    }
+  }
+}
+
+const App = () => {
+  const [data, setData] = useState<Flight[]>([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [isError, setIsError] = useState(false)
+
+  useEffect(() => {
+    setIsLoading(true)
+    fetchFlights()
+        .then((data) => {
+          setData(data)})
+        .catch((error: unknown) => {
+          if(error instanceof Error) {
+            // show error notification
+            setIsError(true)
+          }
+        })
+        .finally(() => {
+          setIsLoading(false)
+          setIsError(false)
+        }
+      )
+  }, [])
+
+  if (!data) {
+    return <div>No flights</div>
+  }
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
+  if (isError) {
+    return <div>Something went wrong</div>
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h2>Flight diary entries</h2>
+      <ul>
+        {data.map(({id, date, weather, visibility}) => {
+          return (
+            <li key={id}>
+              <div><strong>{date}</strong></div>
+              <div>{weather}</div>
+              <div>{visibility}</div>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
   )
 }
 
